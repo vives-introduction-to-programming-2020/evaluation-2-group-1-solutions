3d67f1659c8b26c6bf2979099bdd0ccfb3c5a41f
Nico De Witte <nico.dewitte@outlook.com>
2021-01-13 07:50:57 +0100

    Solve challange FindTheKey

--- FindTheKey/FindTheKey/KeyValueParser.cs
+++ FindTheKey/FindTheKey/KeyValueParser.cs
@@ -15,7 +15,37 @@ namespace FindTheKey
             // TODO - Check if "text" is valid. If not return empty string
             // TODO - Get the value ("Nico De Witte" in the example case) from the string
 
-            return "Something";     // TODO - Replace this with the actual result
+            if (!IsValid(text))
+            {
+                return "";
+            }
+
+            ///////////////////////////////////
+            /// Option 1 - Basic constructs ///
+            ///////////////////////////////////
+
+            //int iColon = 0;
+            //while (text[iColon] != ':')
+            //{
+            //    iColon++;
+            //}
+
+            //iColon += 2;    // For space after colon
+
+            //String value = "";
+            //for (int i = iColon; i < text.Length; i++)
+            //{
+            //    value += text[i];
+            //}
+            //return value;       // Or value.Trim(); to remove space in front
+
+
+            ///////////////////////////////////////
+            /// Option 2 - Using String methods ///
+            ///////////////////////////////////////
+
+            int iColon = text.IndexOf(':');
+            return text.Substring(iColon + 2, (text.Length - iColon - 2));
         }
 
         public bool IsValid(string text)
@@ -23,7 +53,27 @@ namespace FindTheKey
             // TODO: Check if "text" is a valid key-value string.
             // TODO: Return 'false' if string is empty or does not contain a colon :
 
-            return false;     // TODO - Replace this with the actual result
+
+            ///////////////////////////////////
+            /// Option 1 - Basic constructs ///
+            ///////////////////////////////////
+
+            //foreach (var c in text)
+            //{
+            //    if (c == ':')
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;       // If you get here, there is no colon
+
+
+            ///////////////////////////////////////
+            /// Option 2 - Using String methods ///
+            ///////////////////////////////////////
+            
+            int iColon = text.IndexOf(':');
+            return iColon >= 0;
         }
     }
 }
