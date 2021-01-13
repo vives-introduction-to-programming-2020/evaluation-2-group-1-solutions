16b6fee48647310c900d67eccbca58fe17648a70
Nico De Witte <nico.dewitte@outlook.com>
2021-01-13 07:55:27 +0100

    Solve HowManyAreThere

--- HowManyAreThere/HowManyAreThere/ArrayHelper.cs
+++ HowManyAreThere/HowManyAreThere/ArrayHelper.cs
@@ -11,7 +11,16 @@ namespace HowManyAreThere
             // TODO - Count how many times "value" occurs in "numbers"
             // TODO - Make sure to return 0 if the array is empty
 
-            return 123;     // TODO - Replace this with the actual result
+            int count = 0;
+            foreach (int number in numbers)     // Or can use for-loop
+            {
+                if (number == value)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         public int Minimum(int[] numbers)
@@ -19,7 +28,21 @@ namespace HowManyAreThere
             // TODO - Search for the smallest value in "numbers"
             // TODO - Make sure to return 0 if the array is empty
 
-            return 123;     // TODO - Replace this with the actual result
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int currentMinimum = numbers[0];    // First is smallest to start with
+            foreach (int number in numbers)     // Could also use for starting from first element
+            {
+                if (number < currentMinimum)
+                {
+                    currentMinimum = number;
+                }
+            }
+
+            return currentMinimum;
         }
     }
 }
