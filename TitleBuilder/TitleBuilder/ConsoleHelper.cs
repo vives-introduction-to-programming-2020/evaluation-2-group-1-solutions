9095a48f3fd683534e3490b768fc1a1c2a34b7bf
Nico De Witte <nico.dewitte@outlook.com>
2021-01-13 08:10:20 +0100

    Solve TitleBuilder

--- TitleBuilder/TitleBuilder/ConsoleHelper.cs
+++ TitleBuilder/TitleBuilder/ConsoleHelper.cs
@@ -15,7 +15,57 @@ namespace TitleBuilder
             // # ------------------------------------- #
             // #########################################
 
-            return "";    // TODO - Replace this with the actual result
+            String output = "";
+
+            //////////////////////////////////
+            /// Option 1 - No extra method ///
+            //////////////////////////////////
+
+            //// First hashtag line
+            //for (int i = 0; i < text.Length + 4; i++)
+            //{
+            //    output += "#";
+            //}
+            //output += "\n";     // Newline
+
+            //// text
+            //output += "# " + text + " #\n";
+
+            //// ------------
+            //output += "# ";
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    output += "-";
+            //}
+            //output += " #\n";
+
+            //// Second hashtag line
+            //for (int i = 0; i < text.Length + 4; i++)
+            //{
+            //    output += "#";
+            //}
+
+            //////////////////////////////////////////////////
+            /// Option 2 - Nice and DRY with custom method ///
+            //////////////////////////////////////////////////
+
+            output += CreateCharacterLine('#', text.Length + 4) + "\n";
+            output += $"# {text} #\n";
+            output += $"# {CreateCharacterLine('-', text.Length)} #\n";
+            output += CreateCharacterLine('#', text.Length + 4);
+
+            return output;
+        }
+
+        // Cleanest solution was to create extra method
+        private string CreateCharacterLine(char symbol, int count)
+        {
+            string output = "";
+            for (int i = 0; i < count; i++)
+            {
+                output += symbol;
+            }
+            return output;
         }
     }
 }
